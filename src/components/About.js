2ac97eec6fc7a2209882d0cd7f788342baa7d8f8
Omas Ajiri <jicintech@gmail.com>
2020-11-28 16:47:42 +0100

    feat: added a contact page

--- src/components/About.js
+++ src/components/About.js
@@ -7,12 +7,12 @@ const About = () => {
             <div className="underline"></div>
             <div className="about-content">
                 <h4>Our Vision</h4>
-                <p>We see a path to creating wealth for our people through constructive saving. we call it constructive in that unlike traditional saving persons get to that seemlessly with the help of their phones </p>
+                <p>We see a path to creating wealth for our people through constructive saving. we call it constructive in that unlike traditional saving persons get to do that seemlessly with the help of their phones </p>
                 <h4>Our Mission</h4>
                 <p>It is possible for everyone to create wealth when show how. It is our mission to make sure we show people just how to create wealth and to be financially free. We know this is possible and want to pursue it for everyone! </p>
 
                 <h4>Our conviction</h4>
-                <p>Our conviction is born from keen observation of those who we considered very successful in our society and careful study of their source of wealth all point to a clear</p>
+                <p>Our conviction is born from keen observation of those who we considered very successful in our society and careful study of their source of wealth all pointing to saving and investment. This made our resolution so strong and our conviction very ascertained</p>
             </div>
         </article>
     )
--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,7 +2,28 @@ import React from 'react';
 
 const Contact = () => {
     return (
-        <div>Hello Project!</div>
+        <article className="contact">
+            <h2>Contact Us</h2>
+            <div className="underline"></div>
+        <form className="contact-form">
+            <h4>reach out to us today</h4>
+           <div className="form-control">
+                <label for="name">Your Full Name:</label>
+                <input type="text" name="name" placeholder="Enter your name" />
+            </div>
+            <div className="form-control">
+                <label for="name">Your Email:</label>
+                <input type="text" name="name" placeholder="Enter your email" />
+            </div>
+            <div className="form-control">
+                <label for="message">Message:</label>
+                <textarea name="message" placeholder="Start typing..." ></textarea>
+            </div>
+            <div className="btn-container">
+                <button className="btn submit-btn" type="submit">Send</button>
+            </div>
+        </form>
+        </article>
     )
 }
 
